74b914410f0b78acdf4d1a6ea136c1c86a52fee1
joaomboni <joaoboni017@gmail.com>
2022-01-02 21:02:00 -0300

    adicionando o bootstrap e buscando com o jquery

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 function consultaCep(){
     var cep = document.getElementById("cep").value;
     
-    var url = `https://viacep.com.br/ws/${cep}/json/`
+    var url = `https://viacep.com.br/ws/${cep}/json/`;
     
     console.log(url);
     
@@ -12,12 +12,7 @@ function consultaCep(){
         type: "GET",
         success: function(response){
             console.log(response);
-            document.getElementById("bairro").innerHTML = response.bairro;
-            document.getElementById("complemento").innerHTML = response.complemento;
-            document.getElementById("ddd").innerHTML = response.ddd;
-            document.getElementById("localidade").innerHTML = response.localidade;
-            document.getElementById("logradouro").innerHTML = response.logradouro;
-            document.getElementById("uf").innerHTML = response.uf
+            
         }
     })
 }
